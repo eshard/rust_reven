434edf542d2155a6e8ec4abb06a752a8d4efa8f3
Georges Gagnerot <georges.gagnerot@eshard.com>
2024-04-18 16:25:56 +0200

    Updated challenge instructions with a REAME

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,3 @@
-/*
-You are trying to store multiple callbacks in MyStruct and call them with a concrete type in the do_something method.
-Specializing MyStruct (which is parametrized with the argument type of the closure) introduces the following lifetime issue :
-
-cannot infer an appropriate lifetime for borrow expression due to conflicting
-requirements
-
-note: ...so that reference does not outlive borrowed content
-note: expected `&MyCallbackData<'a>`
-	  found `&MyCallbackData<'_>`
-
-Please detail what is the problem, and how you would deal with it.
-*/
-
-
 trait CallbackData {}
 
 #[derive(Debug)]
